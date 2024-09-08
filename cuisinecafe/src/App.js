286cea3cb7e82fa65a7df3a2f02fc037d69c4772
YujenM <maharjanyuzen@gmail.com>
2024-09-08 16:20:14 +0545

    React Router added

--- cuisinecafe/src/App.js
+++ cuisinecafe/src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import * as icon from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import * as icon from '@fortawesome/free-solid-svg-icons';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';  // Adjusted for version 6
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
 
-      <h1 className=''> La Cuisine Cafe</h1>
-      <FontAwesomeIcon icon={icon.faChartLine} />
+      {/* <h1 className=''> La Cuisine Cafe</h1>
+      <FontAwesomeIcon icon={icon.faChartLine} /> */}
     </div>
   );
 }
