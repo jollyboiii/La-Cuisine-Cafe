ebfa929d88f74ac9160ab00edd092c95b2aadd51
YujenM <maharjanyuzen@gmail.com>
2024-09-12 12:19:09 +0545

    Footer component added

---
+++ cuisinecafe/src/components/Footer.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import '../Css/Footer.css';
+import instagramimg from '../Images/instagram.png'
+import tiktok from '../Images/tiktok.png';
+
+function Footer() {
+  return (
+    <div className='mainfooter'>
+      <div className="footerbgimage"/>
+      <div className="footermaininfo container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="footerinfo mt-10">
+          <h2>Buisness Information</h2>
+          <div className='footerinfop mt-2'> 
+            <p>Address: Cafe's Address</p>
+            <p>Phone: 0433686477</p>
+            <p>Email:"info@lacuisinecafe.com.au</p>
+            <p>Business Hours: "Open Daily: 7 am - 3 pm"</p>
+          </div>
+        </div>
+        <div className="footerinfo mt-10">
+          <h2>Buisness Information</h2>
+          <div className='footerinfop mt-2'> 
+            <Link>Menu</Link><br/>
+            <Link>Catering Services</Link><br/>
+            <Link>Rental Kitchen</Link><br/>
+            <Link>Contact Us</Link> <br/>
+          </div>
+        </div>
+        <div className="footerinfo mt-10">
+          <h2>Quick Link</h2>
+          <div className='footerimg mt-2'> 
+            <img src={instagramimg} alt="Instagram" />
+            <img src={tiktok} alt="tiktok" />
+          </div>
+        </div>
+
+      </div>
+
+      <div className="footerbottom">
+        <p>© 2024 La Cuisine Cafe. All rights reserved.</p>
+        <span>|</span>
+        <p>Website designed by Roobits.</p>
+
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
