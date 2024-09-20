d757d505182f0905b1b3c5b897c37ff2abecebd2
YujenM <maharjanyuzen@gmail.com>
2024-09-20 18:09:49 +0545

    menu added

---
+++ cuisinecafe/src/components/Menu2.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import menu2 from '../list/menu2'
+import '../Css/menu.css';
+
+function Menu2() {
+  return (
+    <div className="menu-container">
+      <div className="menu-inner-container mx-auto mt-2 sm:container">
+        <h2>Artisan Doughnuts</h2>
+        <div className="menu-underline"></div>
+        <div className="menu-background1">
+          <div className="menu-inner-background">
+            <div className="menu-grid">
+              {menu2.map((item, index) => (
+                <div key={index} className='menu-card'>
+                  <div className='menu-card-inner'>
+                    <h3>{item.name}</h3>
+                    <p>{item.price}</p>
+                  </div>
+                  <p>{item.description}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Menu2
